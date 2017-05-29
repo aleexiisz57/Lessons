6fc97ce3a35b521c82a5525130588e86bc886d9b
aleexiisz57 <aleexiisz57@gmail.com>
2017-05-29 09:46:10 -0400

    first math problem

---
+++ Lesson-1/math.py
@@ -0,0 +1,9 @@
+print "if 10 students want apples and i only have 16 apples, can everyone have two apples?"
+print 5 + 5 > 16
+print "How many more apples do we need so that everyone can have 2?"
+print 20 - 16
+print "three kids decided they didnt want apples. How many apples do we have extra?"
+print (3 * 2)
+print "there is also 32 O.J cartons, and and 16 students, would they be able to have two juices?"
+print 32 > 16
+print 32 / 2
