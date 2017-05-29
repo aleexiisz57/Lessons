9df281fda033f24b93647c0b6d3747b8ea96ead4
aleexiisz57 <aleexiisz57@gmail.com>
2017-05-29 08:17:23 -0400

    first commit

---
+++ Lesson 1/ex1.py
@@ -0,0 +1,10 @@
+print "Hello world"
+print "Hello Aagain"
+print "i Like tying this"
+print "this is fun"
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this'
+# I managed to make this script from the book and uploaded it to github
+print "This is going to be very fun"
+#
