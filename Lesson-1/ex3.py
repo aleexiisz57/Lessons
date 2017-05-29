65a6b7f0378a33a0bd447e3be0555d795ed4fca7
aleexiisz57 <aleexiisz57@gmail.com>
2017-05-29 09:06:25 -0400

    exercise 3

---
+++ Lesson-1/ex3.py
@@ -0,0 +1,24 @@
+#this line prints text
+print "i will now count my chickens:"
+#This iines do math by fallowing "PEMDAS"?
+print "Hens", 25 + 30 / 6
+print "Rosters", 100 - 25 ^ 3 % 4
+#prints text into the screen
+print "now i will count the eggs:"
+#More PEMDAS 
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#these lines show an equation happening
+print "is this true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+#this is additon 
+print "What is 3 + 2?", 3 + 2
+# this is subtraction
+print "What is 5 - 7?", 5 - 7
+#line of plane text
+print "oh, that's why it's False"
+
+print "How about some more."
+#equations proving why 5 is greater and qual to -2
+print "is it greater?", 5 > -2 
+print "is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
