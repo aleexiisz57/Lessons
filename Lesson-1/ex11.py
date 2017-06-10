0e39d126407ec918db2a58b95d79b682d4d67e1e
aleexiisz57 <aleexiisz57@gmail.com>
2017-06-09 20:24:33 -0400

    11

--- Lesson-1/ex11.py
+++ Lesson-1/ex11.py
@@ -1,3 +1,4 @@
+#here we print a question and then we get a input action where you can type something.
 print "How old are you?",
 age = raw_input()
 print "How tall are you",
@@ -5,4 +6,5 @@ height = raw_input()
 print "How much do you weight?",
 weight = raw_input()
 
+#Here we use the format character "%r" to input the variables into the string.
 print "So, you're %r old, %r tall and %r heavy." % ( age, height, weight)
