4e81883e5f0255de9299cf43c4f518fb18edfa8c
aleexiisz57 <aleexiisz57@gmail.com>
2017-06-09 21:17:20 -0400

    .

--- Lesson-1/ex11.py
+++ Lesson-1/ex11.py
@@ -7,4 +7,23 @@ print "How much do you weight?",
 weight = raw_input()
 
 #Here we use the format character "%r" to input the variables into the string.
-print "So, you're %r old, %r tall and %r heavy." % ( age, height, weight)
+print "So, you're %r old, %r tall and %r heavy." % (
+	age, height, weight)
+	
+	
+#What "raw_input" does is it gets input from the user and returns the data input by the user in a string.
+
+#Example_1
+oranges  = raw_input("How many oranges did you count? ")
+print "I counted %s oranges." % oranges
+
+#Example_2
+print "What country are you from?",
+country = raw_input()
+print "What's your favorite color?"
+color = raw_input()
+print "What's your favorite activity?",
+activity = raw_input()
+
+print "Your country is %r, your favorite color is %r and your favorite activity is %r." % (
+	country, color, activity)
